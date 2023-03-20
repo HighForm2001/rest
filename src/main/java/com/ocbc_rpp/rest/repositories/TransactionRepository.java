9e3806f6ea70f09de9c59eb1138954e1407df178
Chin Jing Yang <jing.yang.chin@fusionex.local>
2023-03-20 22:38:22 +0800

    Testing native ql

--- src/main/java/com/ocbc_rpp/rest/repositories/TransactionRepository.java
+++ src/main/java/com/ocbc_rpp/rest/repositories/TransactionRepository.java
@@ -20,7 +20,7 @@ public interface TransactionRepository extends JpaRepository<Transaction,Long> {
     @Query(value = "select new com.ocbc_rpp.rest.models.TransactionReportSum(c.name,c.accountNo, cast(t.transactionDate as localdate),SUM(case when t is null then 0 else t.amount end)) FROM Customer c left join c.transactionsMade t group by c.accountNo, c.name, cast(t.transactionDate as localdate ) order by c.accountNo")
     List<TransactionReportSum> leftJoinAndSumJPQL();
 
-    @Query(value = "SELECT t.from_acc_id, c.name, DATE(t.transaction_date), SUM(t.amount) FROM transaction_t t left join customer_t c on t.from_acc_id = c.account_no group by t.from_acc_id, c.name, Date(t.transaction_date)",nativeQuery = true)
+    @Query(value = "SELECT t.from_acc_id, c.name, DATE(t.transaction_date), COALESCE(SUM(t.amount),0) as total_amount FROM transaction_t t right join customer_t c on t.from_acc_id = c.account_no group by t.from_acc_id, c.name, Date(t.transaction_date) order by t.from_acc_id",nativeQuery = true)
     List<TransactionReportSumInterface> findGroupByReportWithNativeQuery();
 
     List<Transaction> findByCreatorAndReceiverNot(Customer receiver, Customer creator);
